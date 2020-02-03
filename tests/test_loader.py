080562b477764ccdbf53eb06a864c9c3c5e9f8c9
Even <even.kallevik@gmail.com>
2020-02-03 20:20:18 +0100

    Add data for tests

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,30 +1,18 @@
 from unittest import mock
-
 import pytest
 
 import loader
 
 
 @pytest.fixture()
-@mock.patch("loader.BASE_DIR", "../../flexible_employee_scheduling_data")
+@mock.patch.object(loader, "BASE_DIR", "./tests")
 def data():
     return loader.load_xml_into_dict()
 
 
-@pytest.fixture()
-@mock.patch("loader.BASE_DIR", "../../flexible_employee_scheduling_data")
-def real_data():
-    return loader.load_xml_into_dict(real_mode=True)
-
-
-def test_loader_can_load_artificial_problem(data):
+def test_loader_can_load_data(data):
 
     assert type(data) == dict
-
-
-def test_loader_can_load_real_problem(real_data):
-
-    assert type(real_data) == dict
 
 
 def test_loader_returns_schedule_info(data):
@@ -38,17 +26,6 @@ def test_loader_returns_schedule_info(data):
 def test_loader_returns_employees_for_test_data(data):
 
     employees = loader.get_employees(data)
-
-    assert type(employees) == dict
-    assert len(employees) == 1
-    assert type(employees["ScheduleRow"]) == list
-
-
-def test_loader_returns_employees_for_real_data(real_data):
-
-    employees = loader.get_employees(data)
-
-    print(employees)
 
     assert type(employees) == dict
     assert len(employees) == 1
