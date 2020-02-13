31cdbc86aaf71850d1fb611305faee346769c663
Even <even.kallevik@gmail.com>
2020-02-13 17:30:04 +0100

    Add constraints for max demand and deviation

--- source/esp_with_shifts.py
+++ source/esp_with_shifts.py
@@ -9,6 +9,15 @@ def add_mu_variable(model, competencies, time_periods):
     return model.addVars(competencies, time_periods, vtype=GRB.INTEGER, name='mu')
 
 
+def add_delta_variables(model, competencies, time_periods):
+    deltas = [None, None]
+    deltas[0] = model.addVars(competencies, time_periods, vtype=GRB.INTEGER, name='delta_plus')
+    deltas[1] = model.addVars(competencies, time_periods, vtype=GRB.INTEGER, name='delta_minus')
+
+    return deltas
+
+# todo: convert min_demand to tupledict
+# todo: add competencies
 def add_minimum_demand_constraint(model, y, employees, min_demand, mu, competencies, time_periods):
 
     model.addConstrs((
@@ -19,5 +28,31 @@ def add_minimum_demand_constraint(model, y, employees, min_demand, mu, competenc
         name='minimum_demand_coverage')
 
     model.update()
+
+
+def add_maximum_demand_constraint(model, max_demand, min_demand, mu, competencies, time_periods):
+
+    model.addConstrs((
+        mu[c, t] <= max_demand[c][t] - min_demand[c][t]
+        for c in competencies
+        for t in time_periods),
+        name="maximum_demand_coverage"
+    )
+
+    model.update()
+
+
+def add_deviation_from_ideal_demand_constraint(model, min_demand, ideal_demand, mu, delta_plus, delta_minus,
+                                               competencies, time_periods):
+
+    model.addConstrs((
+        mu[c, t] + min_demand[c][t] - ideal_demand[c][t]
+        == delta_plus[c, t] - delta_minus[c, t]
+        for c in competencies
+        for t in time_periods),
+        name="deviation_from_ideal_demand")
+
+    model.update()
+
 
 
