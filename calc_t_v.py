564ea3bff6e1f337a8567fd545b318f98ba7cbfa
Håkon Grov <Hakongrov@gmail.com>
2020-02-20 11:55:59 +0100

    Includes employees, weekly and daily rest in loader

--- calc_t_v.py
+++ calc_t_v.py
@@ -1,4 +1,6 @@
 import xml_loader.xml_loader as xl
+from datetime import datetime
+
 
 
 
@@ -6,12 +8,10 @@ def get_time_periods():
     time_periods = []
     days = xl.get_demand()
     for day in days:
-        for time in range(len(days[day].start)):
-            imp = ('{:.5}'.format(str(days[day].start[time])))
-            #print(days[day].end[time] - days[day].start[time])
+        for time in days[day].start:
+            imp = ('{:.5}'.format(str(time)))
             if imp not in time_periods:
                 time_periods.append(imp)
-
         
         for time in days[day].end:
             imp = ('{:.5}'.format(str(time)))
@@ -19,6 +19,10 @@ def get_time_periods():
                 time_periods.append(imp)
 
 
+        for time in range(len(days[day].start)):
+            tdelta = datetime.combine(day, days[day].end[time]) - datetime.combine(day,days[day].start[time]) 
+            print(tdelta)
+        
     print(time_periods)
 
 get_time_periods()
--- xml_loader/xml_loader.py
+++ xml_loader/xml_loader.py
@@ -1,9 +1,44 @@
 import xml.etree.ElementTree as ET
-import datetime
+from datetime import date, time, timedelta, datetime
 from pathlib import Path
 
 data_folder = Path(__file__).resolve().parents[2]
+today = date.today()
+root = ET.parse(data_folder / 'flexible_employee_scheduling_data/xml data/Real Instances/rproblem4.xml').getroot()
 
+
+class weekly_rest_rule():
+    def __init__(self, hours, rest_id):
+        self.rest_id = rest_id
+        self.hours = hours
+
+class daily_rest_rule():
+    def __init__(self, hours, rest_id):
+        self.rest_id = rest_id
+        self.hours = hours
+
+class employee():
+    def __init__(self, nbr):
+        self.id = nbr
+        self.weekly_rest_hours = None
+        self.daily_rest_hours = None
+        self.competency = None
+        self.contracted_hours = None
+
+    def add_daily_rest(self, daily_rest):
+        pass
+
+    def add_weekly_rest(self, daily_rest):
+        pass
+        
+    def set_comptency(self, competency):
+        pass
+        
+    def set_contracted_hours(self, hours):
+        self.contracted_hours = hours
+
+    def __str__(self):
+        return self.id
 
 class demand():
 
@@ -14,35 +49,36 @@ class demand():
         self.minimum = []
         self.maks = [] 
         self.ideal = []
+        self.time_delta = []
 
     def add_info(self, start, end, maksimum, minimum, ideal):
         start = start.split(":")
         end = end.split(":")
-        self.start.append(datetime.time(int(start[0]),int(start[1])))
-        self.end.append(datetime.time(int(end[0]),int(end[1])))
+        start = time(int(start[0]), int(start[1]))
+        end = time(int(end[0]), int(end[1]))
+        self.start.append(start)
+        self.end.append(end)
         self.minimum.append(int(minimum))
         self.maks.append(int(maksimum))
         self.ideal.append(int(ideal))
+        self.time_delta.append(datetime.combine(today, end) - datetime.combine(today,start))
 
     def __str__(self):
         return(self.demand_id)
 
 
 def get_demand():
-    root = ET.parse(data_folder / 'flexible_employee_scheduling_data/xml data/Real Instances/rproblem3.xml').getroot()
     demands = []
-    i = 0
-
     for DemandDefinition in root.findall('Demands/DemandDefinitions/DemandDefinition'):
-        demands.append(demand(DemandDefinition.find("DayDemandId").text))
+        dem = demand(DemandDefinition.find("DayDemandId").text)
         for row in DemandDefinition.find("Rows").findall("Row"):
             start = row.find("TimeStart").text
             end = row.find("TimeEnd").text
             maksimum = row.find("Max").text
             minimum = row.find("Min").text
             ideal = row.find("Ideal").text
-            demands[i].add_info(start, end, maksimum, minimum, ideal)
-        i+=1
+            dem.add_info(start, end, maksimum, minimum, ideal)
+        demands.append(dem)
 
     days_with_demand = {}
     for day in root.findall('Demands/DayDemandList/DayDemand'):
@@ -50,3 +86,40 @@ def get_demand():
             if obj.demand_id == day.find("DayDemandId").text:
                 days_with_demand[day.find("DayIndex").text] = obj
     return days_with_demand
+
+
+
+def get_employees():
+    employees = []
+    for schedule_row in root.findall('SchedulePeriod/ScheduleRows/ScheduleRow'):
+        employee_id = schedule_row.find("RowNbr").text
+        contracted_hours = float(schedule_row.find("WeekHours").text)
+
+        emp = employee(employee_id)
+        emp.set_contracted_hours(contracted_hours)
+        employees.append(emp)
+    return employees
+
+
+def get_weekly_rest_rules():
+    weekly_rest_rules = []
+    for weekly_rule in root.findall('Configuration/WeeklyRestRules/WeeklyRestRule'):
+        rest_id = weekly_rule.find("Id")
+        hours = weekly_rule.find("MinRestHours")
+        rule = weekly_rest_rule(hours, rest_id)
+        weekly_rest_rules.append(rule)
+
+def get_daily_rest_rules():
+    daily_rest_rules = []
+    for daily_rule in root.findall('Configuration/DailyRestRules/DailyRestRule'):
+        rest_id = daily_rule.find("Id")
+        hours = daily_rule.find("MinRestHours")
+        rule = daily_rest_rule(hours, rest_id)
+        daily_rest_rules.append(rule)
+
+
+get_demand()
+get_daily_rest_rules()
+get_weekly_rest_rules()
+for e in get_employees():
+    print(e.contracted_hours)
