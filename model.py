4f27fdf09fa8f36796f9ab0a67ceff3d7b3dcd44
Håkon Grov <Hakongrov@gmail.com>
2020-03-01 18:21:30 +0100

    Important bug fix! A mistake in the mathematical model that ruined the partial weekend contraint

--- model.py
+++ model.py
@@ -74,13 +74,13 @@ model.addConstrs((
     for c in competencies),
 name="deviation_from_ideel_demand")
 
-# model.addConstrs((
-#     quicksum(x[e,t,v] 
-#     for t,v in shifts_at_day[i]) 
-#     <= 1 
-#     for e in employees 
-#     for i in days), 
-#     name="cover_maximum_one_shift")
+model.addConstrs((
+    quicksum(x[e,t,v] 
+    for t,v in shifts_at_day[i]) 
+    <= 1 
+    for e in employees 
+    for i in days), 
+    name="cover_maximum_one_shift")
 
 model.addConstrs((
     quicksum(x[e,t_marked,v] 
@@ -90,29 +90,29 @@ model.addConstrs((
     for t in time_periods
 ),name="mapping_shift_to_demand")
 
-# model.addConstrs((
-#     quicksum(y[c,e,t] 
-#     for c in competencies) 
-#     <= 1 
-#     for e in employees 
-#     for t in time_periods
-# ), name="only_cover_one_demand_at_a_time")
+model.addConstrs((
+    quicksum(y[c,e,t] 
+    for c in competencies) 
+    <= 1 
+    for e in employees 
+    for t in time_periods
+), name="only_cover_one_demand_at_a_time")
 
-# model.addConstrs((
-#     quicksum(x[e,t,v] 
-#     for t,v in shifts_at_day[i]) 
-#     == gamma[e,i] 
-#     for e in employees 
-#     for i in days
-# ), name="if_employee_e_works_day_i")
+model.addConstrs((
+    quicksum(x[e,t,v] 
+    for t,v in shifts_at_day[i]) 
+    == gamma[e,i] 
+    for e in employees 
+    for i in days
+), name="if_employee_e_works_day_i")
 
-# model.addConstrs((
-#     quicksum(w[e,t,v] 
-#     for t,v in off_shift_in_week[j]) 
-#     == 1 
-#     for e in employees 
-#     for j in weeks
-# ), name="one_weekly_off_shift_per_day")
+model.addConstrs((
+    quicksum(w[e,t,v] 
+    for t,v in off_shift_in_week[j]) 
+    == 1 
+    for e in employees 
+    for j in weeks
+), name="one_weekly_off_shift_per_day")
 
 # model.addConstrs((
 #     len(t_in_off_shifts[t,v]) * w[e,t,v]
@@ -125,23 +125,23 @@ model.addConstrs((
 #     for t,v in off_shifts
 # ), name="no_work_during_off_shift")
 
-#Alternativ 2 til off_shift restriksjon (restriksjon 1.10). Virker raskere
-# model.addConstrs((
-#     len(shifts_covered_by_off_shift[t,v]) * w[e,t,v] <= 
-#     quicksum(
-#         quicksum(
-#             (1-x[e,t_marked,v_marked]) for c in competencies
-#         ) for t_marked,v_marked in shifts_covered_by_off_shift[t,v]
-#     ) for e in employees for t,v in off_shifts
-# ), name="no_work_during_off_shift")
+# Alternativ 2 til off_shift restriksjon (restriksjon 1.10). Virker raskere
+model.addConstrs((
+    len(shifts_covered_by_off_shift[t,v]) * w[e,t,v] <= 
+    quicksum(
+        quicksum(
+            (1-x[e,t_marked,v_marked]) for c in competencies
+        ) for t_marked,v_marked in shifts_covered_by_off_shift[t,v]
+    ) for e in employees for t,v in off_shifts
+), name="no_work_during_off_shift")
 
-# model.addConstrs((
-#     quicksum(
-#         quicksum(
-#             time_step*y[c,e,t] for t in time_periods
-#         ) for c in competencies
-#     ) + lam[e] == len(weeks)*contracted_hours[e] for e in employees
-# ), name="contracted_hours")
+model.addConstrs((
+    quicksum(
+        quicksum(
+            time_step*y[c,e,t] for t in time_periods
+        ) for c in competencies
+    ) + lam[e] == len(weeks)*contracted_hours[e] for e in employees
+), name="contracted_hours")
 
 # model.addConstrs((
 #     quicksum(
@@ -159,44 +159,44 @@ model.addConstrs((
 #     ) <= 1.4*contracted_hours[e] for e in employees for j in weeks
 # ), name="maximum_weekly_work_hours")
 
-# model.addConstrs((
-#     gamma[e,i] + gamma[e,(i+1)] == ro_sat[e,i] - ro_sun[e,(i+1)] for e in employees for i in saturdays
-# ), name="partial_weekends")
+model.addConstrs((
+    gamma[e,i] - gamma[e,(i+1)] == ro_sat[e,i] - ro_sun[e,(i+1)] for e in employees for i in saturdays
+), name="partial_weekends")
 
-# model.addConstrs((
-#     -gamma[e,i] + gamma[e,(i+1)] - gamma[e,(i+2)] <= q_iso_work[e,(i+1)] for e in employees for i in range(len(days)-2)
-# ), name="isolated_working_days")
+model.addConstrs((
+    -gamma[e,i] + gamma[e,(i+1)] - gamma[e,(i+2)] <= q_iso_work[e,(i+1)] for e in employees for i in range(len(days)-2)
+), name="isolated_working_days")
 
-# model.addConstrs((
-#     gamma[e,i] - gamma[e,(i+1)] + gamma[e,(i+2)] - 1 <= q_iso_off[e,(i+1)] for e in employees for i in range(len(days)-2)
-# ), name="isolated_off_days")
+model.addConstrs((
+    gamma[e,i] - gamma[e,(i+1)] + gamma[e,(i+2)] - 1 <= q_iso_off[e,(i+1)] for e in employees for i in range(len(days)-2)
+), name="isolated_off_days")
 
-# model.addConstrs((
-#     quicksum(
-#         gamma[e,i_marked] for i_marked in range(i, i+L_C_D)
-#     ) - L_C_D <= q_con[e,i] for e in employees for i in range(len(days) - L_C_D)
-# ), name="consecutive_days")
+model.addConstrs((
+    quicksum(
+        gamma[e,i_marked] for i_marked in range(i, i+L_C_D)
+    ) - L_C_D <= q_con[e,i] for e in employees for i in range(len(days) - L_C_D)
+), name="consecutive_days")
 
  
-# model.addConstrs((
-#         f_plus[e] - f_minus[e] ==
-#         weights["rest"] * quicksum(v * w[e,t,v] for t,v in off_shifts)
-#         - weights["contracted hours"] * lam[e]
-#         - weights["partial weekends"] * quicksum(ro_sat[e,j] + ro_sun[e,j] for j in weeks)
-#         - weights["isolated working days"] * quicksum(q_iso_work[e,i] for i in days)
-#         - weights["isolated off days"] * quicksum(q_iso_off[e,i] for i in days)
-#         - weights["consecutive days"] * quicksum(q_con[e,i] for i in days)
-#         for e in employees
-#         ), name="objective_function_restriction")
-# #             #- weights["backward rotation"] * k[e,i]
-# #             #+weights["preferences"] * quicksum(pref[e,t] for t in time_periods) * quicksum(y[c,e,t] for c in competencies)
+model.addConstrs((
+        f_plus[e] - f_minus[e] ==
+        weights["rest"] * quicksum(v * w[e,t,v] for t,v in off_shifts)
+        - weights["contracted hours"] * lam[e]
+        - weights["partial weekends"] * quicksum(ro_sat[e,j] + ro_sun[e,j] for j in weeks)
+        - weights["isolated working days"] * quicksum(q_iso_work[e,i] for i in days)
+        - weights["isolated off days"] * quicksum(q_iso_off[e,i] for i in days)
+        - weights["consecutive days"] * quicksum(q_con[e,i] for i in days)
+        for e in employees
+        ), name="objective_function_restriction")
+#             #- weights["backward rotation"] * k[e,i]
+#             #+weights["preferences"] * quicksum(pref[e,t] for t in time_periods) * quicksum(y[c,e,t] for c in competencies)
         
 
 
 
-# model.addConstrs((
-#     g_plus - g_minus <= f_plus[e] - f_minus[e] for e in employees)
-#     , name="lowest_fairness_score")
+model.addConstrs((
+    g_plus - g_minus <= f_plus[e] - f_minus[e] for e in employees)
+    , name="lowest_fairness_score")
 
 #Objective Function:
 model.setObjective(
@@ -215,7 +215,6 @@ model.setObjective(
         ) for t in time_periods
     ), GRB.MINIMIZE
 )
-
 
 
 # model.setObjective(
