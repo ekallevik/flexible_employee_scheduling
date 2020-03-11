6b61093f5808bbfa9467c08967b5a3a550344ac0
Håkon Grov <Hakongrov@gmail.com>
2020-03-11 15:24:22 +0100

    Fixes the hard constraint model

--- hard_constraint_model.py
+++ hard_constraint_model.py
@@ -1,22 +1,40 @@
 from gurobipy import *
-from xml_loader.xml_loader import *
+from xml_loader.shift_generation import *
 import sys
 
 #MODEL
 model = Model("Employee_scheduling_haakon")
 
 #SETS
-employees, employee_with_competencies, employee_weekly_rest, employee_daily_rest, contracted_hours = get_employee_lists()
+data = load_data("rproblem3")
+(
+    employees, 
+    employee_with_competencies, 
+    employee_weekly_rest, 
+    employee_daily_rest, 
+    contracted_hours) = data["employees"]
+
+(
+    time_step, 
+    time_periods, 
+    time_periods_in_week, 
+    days) = data["time"]
+
+
+demand = data["demand"]
+
+(
+    shifts_covered_by_off_shift,
+    shifts_overlapping_t,
+    shifts, 
+    shifts_at_day) = data["shifts"]
+
+(
+    t_in_off_shifts,
+    off_shifts, 
+    off_shift_in_week) = data["off_shifts"]
+
 competencies = [0]
-time_step = get_time_steps()
-time_periods, time_periods_in_week = get_time_periods()
-demand_min, demand_ideal, demand_max = get_demand_periods()
-shifts, shifts_at_day = get_shift_lists()
-days = get_days()
-shifts_covered_by_off_shift = get_shifts_covered_by_off_shifts()
-shifts_overlapping_t = get_shifts_overlapping_t()
-t_in_off_shifts = get_t_covered_by_off_shifts()
-off_shifts, off_shift_in_week = get_off_shifts()
 weeks = [w for w in range(int(len(days)/7))]
 saturdays = [5 + (i*7) for i in range(len(weeks))]
 L_C_D = 5
@@ -33,21 +51,14 @@ print("#############VARIABLES ADDED#############")
 
 
 #Constraints
-model.addConstrs((quicksum(y[c,e,t] for e in employee_with_competencies[c]) == demand_min[c,t] + mu[c,t]  
+model.addConstrs((quicksum(y[c,e,t] for e in employee_with_competencies[c]) == demand["min"][c,t] + mu[c,t]  
 for c in competencies for t in time_periods),
 name='minimum_demand_coverage')
 
-model.addConstrs((mu[c,t] <= demand_max[c,t] - demand_min[c,t]
+model.addConstrs((mu[c,t] <= demand["max"][c,t] - demand["min"][c,t]
     for c in competencies 
     for t in time_periods),
 name='mu_less_than_difference')
-
-model.addConstrs((
-    mu[c,t] + demand_min[c,t] - demand_ideal[c,t] 
-    == delta_plus[c,t] - delta_minus[c,t] 
-    for t in time_periods 
-    for c in competencies),
-name="deviation_from_ideel_demand")
 
 model.addConstrs((
     quicksum(x[e,t,v] 
@@ -72,14 +83,6 @@ model.addConstrs((
     for e in employees 
     for t in time_periods
 ), name="only_cover_one_demand_at_a_time")
-
-model.addConstrs((
-    quicksum(x[e,t,v] 
-    for t,v in shifts_at_day[i]) 
-    == gamma[e,i] 
-    for e in employees 
-    for i in days
-), name="if_employee_e_works_day_i")
 
 model.addConstrs((
     quicksum(w[e,t,v] 
@@ -120,9 +123,6 @@ model.setObjective(
 
 print("#############RESTRICTIONS ADDED#############")
 
-
-model.write(sys.argv[1] + ".lp")
-model.setParam("LogFile", (sys.argv[1] + ".log"))
+model.setParam("LogFile", "hard_constraint_solution/hard_constraint_log")
 model.optimize()
-model.write(sys.argv[1] + ".sol")
-
+model.write("hard_constraint_solution/hard_constraint_log.sol")
