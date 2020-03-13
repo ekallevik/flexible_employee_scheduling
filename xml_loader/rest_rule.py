b94b0dabd42a38410481ec522ee1fe90261b9c60
Håkon Grov <Hakongrov@gmail.com>
2020-03-13 09:36:59 +0100

    Merges in loader files (#24)
    * Merges in loader files
    
    * get_time_steps() is updated to account for minutes, and bug giving 0.5 in some rproblem-cases is fixed. Note the comments and todos
    
    * Changed event2 to event in get_off_shifts(root)
    
    * Extract tests from another branch
    
    * Auto-linting
    
    * Add necessary files from dev/haakon
    
    * Linting
    
    * Minor cleanup
    
    Co-authored-by: Sondre Nærland <sondrenaerland@gmail.com>
    Co-authored-by: Even <even.kallevik@gmail.com>

---
+++ xml_loader/rest_rule.py
@@ -0,0 +1,11 @@
+class Daily_rest_rule():
+    def __init__(self, hours, rest_id):
+        self.rest_id = rest_id
+        self.hours = int(hours)
+        
+class Weekly_rest_rule():
+    def __init__(self, hours, rest_id):
+        self.rest_id = rest_id
+        self.hours = int(hours)
+
+
