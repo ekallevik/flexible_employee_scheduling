5de7af8952a2da971a7716a4a77e972ea43f82ef
Håkon Grov <hakongrov@Hakons-iMac-Pro.local>
2020-03-20 13:01:33 +0100

    Begun rewriting for normal dicts from gurobi dicts

--- heuristic.py
+++ heuristic.py
@@ -6,6 +6,9 @@ problem_name = "rproblem3"
 data_folder = Path(__file__).resolve().parents[1] / 'flexible_employee_scheduling_data/xml data/Real Instances/'
 root = ET.parse(data_folder / (problem_name + '.xml')).getroot()
 
+
+t_covered_by_shift = get_t_covered_by_shift(root)
+
 model = Optimization_model(problem_name)
 model.add_variables()
 model.add_constraints()
@@ -13,11 +16,15 @@ model.set_objective()
 model.optimize()
 
 
-# x,y,w = convert(model)
-x = model.x
-y = model.y
-w = model.w
-employee_shifts = {em: [(t,v) for t,v in model.shifts if x[em,t,v].x != 0] for em in model.employees}
+x,y,w = convert(model)
+
+employee_shifts = {em: [(t,v) for t,v in model.shifts if x[em,t,v] != 0] for em in model.employees}
+#print(employee_shifts)
+
+#x = model.x
+#y = model.y
+#w = model.w
+#employee_shifts = {em: [(t,v) for t,v in model.shifts if x[em,t,v] != 0] for em in model.employees}
 
 
 # for e in model.employees:
@@ -33,7 +40,7 @@ def calculate_deviation_from_demand():
     delta = {}
     for c in model.competencies:
         for t in model.time_periods:
-            delta[c,t] = abs(sum(model.y[c,e,t].x for e in model.employee_with_competencies[c]) - model.demand["ideal"][c,t])
+            delta[c,t] = abs(sum(y[c,e,t] for e in model.employee_with_competencies[c]) - model.demand["ideal"][c,t])
             # if(delta[c,t] - abs(model.delta["plus"][c,t].x - model.delta["minus"][c,t].x)) != 0:
             #     print(delta[c,t] - abs(model.delta["plus"][c,t].x - model.delta["minus"][c,t].x))
             #     print("Different Delta")
@@ -44,7 +51,7 @@ def calculate_deviation_from_contracted_hours():
     for e in model.employees:
         delta_contracted_hours[e] = (len(model.weeks) * model.contracted_hours[e] - sum(
             sum(
-                model.time_step * y[c,e,t].x for t in model.time_periods
+                model.time_step * y[c,e,t] for t in model.time_periods
             ) for c in model.competencies
         ))
     return delta_contracted_hours
@@ -54,7 +61,7 @@ def calculate_partial_weekends():
     print("Partial Weekends")
     for e in model.employees:
         for i in model.saturdays:
-            partial_weekend[e,i] = abs((sum(model.x[e,t,v].x for t,v in model.shifts_at_day[i]) - sum(model.x[e,t,v].x for t,v in model.shifts_at_day[i+1])))
+            partial_weekend[e,i] = abs((sum(x[e,t,v] for t,v in model.shifts_at_day[i]) - sum(x[e,t,v] for t,v in model.shifts_at_day[i+1])))
             # if(partial_weekend[e,i] != abs(model.ro["sat"][e,i].x - model.ro["sun"][e,(i+1)].x)):
             #     print(str(partial_weekend) + "," + str(abs(model.ro["sat"][e,i].x - model.ro["sun"][e,(i+1)].x)))
             #     print(i)
@@ -65,9 +72,9 @@ def calculate_isolated_working_days():
     isolated_working_days = {}
     for e in model.employees:
         for i in range(len(model.days)-2):
-            isolated_working_days[e,i] = max(0,(-sum(x[e,t,v].x for t,v in model.shifts_at_day[i]) 
-            + sum(x[e,t,v].x for t,v in model.shifts_at_day[i+1]) 
-            - sum(x[e,t,v].x for t,v in model.shifts_at_day[i+2])))
+            isolated_working_days[e,i] = max(0,(-sum(x[e,t,v] for t,v in model.shifts_at_day[i]) 
+            + sum(x[e,t,v] for t,v in model.shifts_at_day[i+1]) 
+            - sum(x[e,t,v] for t,v in model.shifts_at_day[i+2])))
 
             # if(isolated_working_days[e,i] != model.q_iso["work"][e,i].x):
             #     print("Different isolated working days")
@@ -79,9 +86,9 @@ def calculate_isolated_off_days():
     isolated_off_days = {}
     for e in model.employees:
         for i in range(len(model.days)-2):
-            isolated_off_days[e,i+1] = max(0,(sum(x[e,t,v].x for t,v in model.shifts_at_day[i]) 
-            - sum(x[e,t,v].x for t,v in model.shifts_at_day[i+1]) 
-            + sum(x[e,t,v].x for t,v in model.shifts_at_day[i+2])-1))
+            isolated_off_days[e,i+1] = max(0,(sum(x[e,t,v] for t,v in model.shifts_at_day[i]) 
+            - sum(x[e,t,v] for t,v in model.shifts_at_day[i+1]) 
+            + sum(x[e,t,v] for t,v in model.shifts_at_day[i+2])-1))
         # if(isolated_off_days[e,i] != model.q_iso["off"][e,i].x):
         #     print("Different isolated off days")
     return isolated_off_days
@@ -91,7 +98,7 @@ def calculate_consecutive_days():
     for e in model.employees:
         for i in range(len(model.days)-model.L_C_D):
             consecutive_days[e,i] = max(0,(sum(
-                sum(x[e,t,v].x for t,v in model.shifts_at_day[i_marked]) 
+                sum(x[e,t,v] for t,v in model.shifts_at_day[i_marked]) 
             for i_marked in range(i,i+model.L_C_D)))- model.L_C_D)
             
             # if(consecutive_days[e,i] != model.q_con[e,i].x):
@@ -107,7 +114,7 @@ def calculate_f():
     delta_c = calculate_deviation_from_contracted_hours()
     f = {}
     for e in model.employees:
-        f[e] = (sum(v * w[e,t,v].x for t,v in model.off_shifts)
+        f[e] = (sum(v * w[e,t,v] for t,v in model.off_shifts)
             - delta_c[e]
             - sum(partial_weekend[e,i] for i in model.saturdays)
             - sum(q_iso_work[e,i] for i in range(len(model.days)-2))
@@ -119,15 +126,17 @@ def calculate_f():
 def calculate_objective_function():
     delta = calculate_deviation_from_demand()
     delta_c = calculate_deviation_from_contracted_hours()
-    lowest_contracted_hours(delta_c, delta)
+    #lowest_contracted_hours(delta_c, delta)
    # for e in model.employees:
     #    print(str(delta_c[e]) + ", index: " + str(e)) 
     f = calculate_f()
     g = min(f.values())
+    print(g)
     objective =    (sum(f[e] for e in model.employees)
                     + g
                     - sum(sum(delta[c,t] for t in model.time_periods) for c in model.competencies)
                     )
+    return objective
 
     #print(objective)
 def cover_minimum_demand():
@@ -181,15 +190,14 @@ def lowest_contracted_hours(delta_c, delta):
     for i in model.days:
         if sum(model.x[employee,t,v].x for t,v in model.shifts_at_day[i]) != 0:
             working_days.append(i)
-    print(working_days)
     maximum_deviation_from_demand = {}
     for i in model.days: 
         if i not in working_days:
             for shift in model.shifts_at_day[i]:
                 maximum_deviation_from_demand[shift] = sum(delta[0,t] for t in t[shift])
-    print(maximum_deviation_from_demand)
+    #print(maximum_deviation_from_demand)
     placement = max(maximum_deviation_from_demand, key=maximum_deviation_from_demand.get)
-    print(x[employee,placement[0], placement[1]])
+    #print(x[employee,placement[0], placement[1]])
     #Remember to set y when you set x values as these should always be mapped
    # print(x[employee, placement[0], placement[1]].set(1))
    # delta2 = calculate_deviation_from_contracted_hours()
@@ -200,12 +208,45 @@ def lowest_contracted_hours(delta_c, delta):
 def remove_partial_weekends():
     partial_weekends = calculate_partial_weekends()
     actual_partial_weekends = [key for key, value in partial_weekends.items() if value != 0]
-    print(actual_partial_weekends)
+    partial_weekend_shifts = {}
 
+    for e,i in actual_partial_weekends:
+        partial_weekend_shifts[e,i] = []
+        for shift in employee_shifts[e]:
+            if shift[0] >= 24*i and shift[0] <= 24*(i+2):
+                partial_weekend_shifts[e,i].append(shift)
+    print(partial_weekend_shifts)
+
+
+
+    # first_half = int(len(actual_partial_weekends)/2)
+    # for s in range(first_half):
+    #     print(s)
+    #     e,i = actual_partial_weekends[s]
+    #     for shift in employee_shifts[e]:
+    #         if shift[0] >= 24*i and shift[0] <= 24*(i+2):
+    #             set_x(e,shift[0], shift[1], 0)
+
+    # for s in range(first_half,len(actual_partial_weekends)):
+    #     print(actual_partial_weekends[s])
+        
+
+    #print(actual_partial_weekends)
+
+
+def set_x(e, t, v, value):
+    x[e,t,v] = value
+    
+    #Need a smarter solution for choosing competency
+    for t in t_covered_by_shift[t,v]:
+        y[0,e,t] = value
+
+#print(calculate_objective_function())
 #print(calculate_partial_weekends())
-#remove_partial_weekends()
+remove_partial_weekends()
 
-convert(model)
+#print(calculate_objective_function())
+#convert(model)
 
 
 
