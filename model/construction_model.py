f73c1922c53c6dad1e2c91ab709f671a669ab7f4
Sondre Nærland <sondrenaerland@gmail.com>
2020-03-26 12:16:42 +0100

    Added construction model that optimizes based on deviation from ideal demand

---
+++ model/construction_model.py
@@ -0,0 +1,41 @@
+from model.base_constraints import BaseConstraints
+from model.base_model import BaseModel
+from model.base_variables import BaseVariables
+from model.construction_objective import ConstructionObjective
+from utils import weights
+
+
+class ConstructionModel(BaseModel):
+    def __init__(self, name, problem):
+        super(ConstructionModel, self).__init__(name, problem)
+
+        self.weights = weights.get_weights()
+
+        self.var = BaseVariables(
+            model=self.model,
+            competencies=self.competencies,
+            staff=self.staff,
+            time_set=self.time_set,
+            shift_set=self.shift_set,
+            off_shift_set=self.off_shift_set,
+        )
+
+        self.constraints = BaseConstraints(
+            model=self.model,
+            var=self.var,
+            competencies=self.competencies,
+            staff=self.staff,
+            demand=self.demand,
+            time_set=self.time_set,
+            shift_set=self.shift_set,
+            off_shift_set=self.off_shift_set,
+        )
+
+        self.objective = ConstructionObjective(
+            model=self.model,
+            var=self.var,
+            weights=self.weights,
+            competencies=self.competencies,
+            staff=self.staff,
+            time_set=self.time_set,
+        )
---
+++ model/construction_objective.py
@@ -0,0 +1,24 @@
+from gurobipy.gurobipy import GRB, quicksum
+
+
+class ConstructionObjective:
+    def __init__(self, model, var, weights, competencies, staff, time_set):
+
+        self.model = model
+
+        self.competencies = competencies
+        self.employees = staff["employees"]
+        self.time_periods = time_set["periods"][0]
+
+        self.add_objective(weights, var.delta)
+
+    def add_objective(self, weights, delta):
+
+        self.model.setObjective(
+            - weights["demand_deviation"]
+            * quicksum(
+                quicksum(delta["plus"][c, t] + delta["minus"][c, t] for t in self.time_periods)
+                for c in self.competencies
+            ),
+            GRB.MAXIMIZE,
+        )
