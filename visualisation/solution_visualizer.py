3864d7cdd2413cde4ecd365d0b46909409caa3fc
Håkon Grov <hakongrov@gmail.com>
2020-03-27 18:29:33 +0100

    Improves color handling of visualiser

--- visualisation/solution_visualizer.py
+++ visualisation/solution_visualizer.py
@@ -6,13 +6,14 @@ from matplotlib.ticker import MaxNLocator
 import sys
 from pathlib import Path
 import xml.etree.ElementTree as ET
+from colour import Color
 
 #Paths
 data_folder = Path(__file__).resolve().parents[2] / 'flexible_employee_scheduling_data/xml data/Real Instances/'
 solution_folder = Path(__file__).resolve().parents[1]
 loader_path = Path(__file__).resolve().parents[1] 
 sys.path.insert(1, str(loader_path))
-from xml_loader.xml_loader import *
+from xml_loader.xml_loader import get_days_with_demand2
 
 # creating indices
 key_y = re.compile('\d*,\d*,\d*')
@@ -56,7 +57,7 @@ def visualize_demand(min_demand, ideal_demand, max_demand, number_of_days):
         plt.savefig(f'figures/Demand for day={day}')
 
 
-def create_gantt_chart(data, solution):
+def create_gantt_chart(data, solution, maximum, minimum):
     #days = periods * 7
     df = []
     for day in data:
@@ -135,41 +136,24 @@ def create_gantt_chart(data, solution):
                     df.append(dictionary)
             line = file.readline()
 
-# 247, 249, 248
-#  239, 244, 242
-#  231, 239, 236
-#  223, 234, 230
-#  215, 229, 224
-#  207, 224, 217
-#  199, 219, 211
-#  191, 214, 205
-#  183, 209, 199
-# Demand_10='175, 204, 193'
-#  Demand_11='168, 199, 187'
-#  Demand_12='153, 181, 170'
-#  Demand_13='138, 163, 154'
-#  Demand_14='123, 145, 137'
-#  Demand_15='107, 127, 119'
-#  Demand_16='92, 109, 102'
-#  Demand_17='77, 91, 85'
-#  Demand_18='62, 73, 68'
-#  Demand_19='46, 55, 51'
-#  Demand_20='31, 37, 34'
-#  Demand_21='16, 19, 17'
 
 
-    colors = dict(Work='rgb(46, 137, 205)', Off='rgb(198, 47, 105)',
-                  Demand_1='rgb(247, 249, 248)', Demand_2='rgb(239, 244, 242)', Demand_3='rgb(231, 239, 236)',
-                  Demand_4='rgb(223, 234, 230)', Demand_5='rgb(215, 229, 224)', Demand_6='rgb(207, 224, 217)', Demand_7='rgb(199, 219, 211)',
-                  Demand_8='rgb(191, 214, 205)', Demand_9='rgb(183, 209, 199)', Demand_10='rgb(175, 204, 193)', Demand_11='rgb(168, 199, 187)', 
-                  Demand_12='rgb(153, 181, 170)', Demand_13='rgb(138, 163, 154)', Demand_14='rgb(123, 145, 137)', Demand_15='rgb(107, 127, 119)',
-                  Demand_16='rgb(92, 109, 102)', Demand_17='rgb(77, 91, 85)', Demand_18='rgb(62, 73, 68)', Demand_19='rgb(46, 55, 51)', Demand_20='rgb(31, 37, 34)',
-                  Demand_21='rgb(16, 19, 17)', Demand_22='rgb(0, 19, 17)' , Demand_23='rgb(16, 50, 18)' , Demand_24= 'rgb(50, 50, 17)', Demand_25= 'rgb(70, 70, 17)',
-                  Demand_26= 'rgb(16, 80, 80)', Demand_27= 'rgb(16, 90, 90)', Demand_28='rgb(16, 200, 120)', Demand_29='rgb(210, 190, 210)',
-                  Demand_30='rgb(100, 100, 100)', Demand_31='rgb(120, 120, 120)', Demand_32='rgb(140, 140, 140)', Demand_33='rgb(160, 160, 160)')
-
-    fig = ff.create_gantt(df, colors=colors, index_col='Resource', show_colorbar=True, group_tasks=True)
-    print("Not Done")
+    # colors = dict(Work='rgb(46, 137, 205)', Off='rgb(198, 47, 105)',
+    #               Demand_1='rgb(247, 249, 248)', Demand_2='rgb(239, 244, 242)', Demand_3='rgb(231, 239, 236)',
+    #               Demand_4='rgb(223, 234, 230)', Demand_5='rgb(215, 229, 224)', Demand_6='rgb(207, 224, 217)', Demand_7='rgb(199, 219, 211)',
+    #               Demand_8='rgb(191, 214, 205)', Demand_9='rgb(183, 209, 199)', Demand_10='rgb(175, 204, 193)', Demand_11='rgb(168, 199, 187)', 
+    #               Demand_12='rgb(153, 181, 170)', Demand_13='rgb(138, 163, 154)', Demand_14='rgb(123, 145, 137)', Demand_15='rgb(107, 127, 119)',
+    #               Demand_16='rgb(92, 109, 102)', Demand_17='rgb(77, 91, 85)', Demand_18='rgb(62, 73, 68)', Demand_19='rgb(46, 55, 51)', Demand_20='rgb(31, 37, 34)',
+    #               Demand_21='rgb(16, 19, 17)', Demand_22='rgb(0, 19, 17)' , Demand_23='rgb(16, 50, 18)' , Demand_24= 'rgb(50, 50, 17)', Demand_25= 'rgb(70, 70, 17)',
+    #               Demand_26= 'rgb(16, 80, 80)', Demand_27= 'rgb(16, 90, 90)', Demand_28='rgb(16, 200, 120)', Demand_29='rgb(210, 190, 210)',
+    #               Demand_30='rgb(100, 100, 100)', Demand_31='rgb(120, 120, 120)', Demand_32='rgb(140, 140, 140)', Demand_33='rgb(160, 160, 160)')
+    # print(colors)
+    red = Color("#8ff9ff")
+    c = list(red.range_to(Color("#146266"),(maximum-minimum)+1))
+    colors2 = {"Demand_" + str(i): c[i-1].hex for i in range(minimum,maximum+1)}
+    colors2["Work"] = 'rgb(46, 137, 205)'
+    colors2["Off"] = 'rgb(198, 47, 105)'
+    fig = ff.create_gantt(df, colors=colors2, show_colorbar=True, index_col='Resource', group_tasks=True, height=1200, showgrid_x=True)
     fig.show()
 
 
@@ -178,12 +162,10 @@ def main():
     data_folder = Path(__file__).resolve().parents[2] / 'flexible_employee_scheduling_data/xml data/Real Instances/'
     root = ET.parse(data_folder / ('rproblem2.xml')).getroot()
     demand = get_days_with_demand2(root)
-    # case = "base_v3.4.json"
+    maks = max([demand[day].ideal[i] for day in demand for i in range(len(demand[day].start))])
+    minst = min([demand[day].ideal[i] for day in demand for i in range(len(demand[day].start))])
     solution = str(solution_folder / "solution.sol")
-    
-    
-
-    create_gantt_chart(demand, solution)
+    create_gantt_chart(demand, solution, maks, minst)
 
 
 if __name__ == '__main__':
