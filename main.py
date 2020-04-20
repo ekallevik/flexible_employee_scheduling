a2c82771a232c7de750260928af83372af47f4ed
Håkon Grov <hakongrov@gmail.com>
2020-04-20 11:56:47 +0200

    This commit is meant to be merged into master from my branch feat/state_implemenatation. I have decided to split this branch into multiple commits to hopefully make the review process easier. This commit mostly changes state structure and other files needed for this state structure to work.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from model.feasibility_model import FeasibilityModel
 from model.optimality_model import OptimalityModel
 from model.construction_model import ConstructionModel
 from results.converter import Converter
-
+from heuristic.heuristic_calculations import *
 
 def run_heuristic(construction_model="feasibility", problem="rproblem2"):
     """
@@ -22,7 +22,32 @@ def run_heuristic(construction_model="feasibility", problem="rproblem2"):
 
     converter = Converter(candidate_solution)
     converted_solution = converter.get_converted_variables()
-    state = State(converted_solution)
+    
+    #The soft variables, hard variables objective function and f is needed to do delta calculations later on.
+    # These calculations are done using heuristic calculations on the solution gotten from the construction model.
+    # This is why they are needed to be calculated here and placed in the initial state at the beginning. 
+    soft_variables = {
+        "negative_deviation_from_demand": calculate_negative_deviation_from_demand(candidate_solution, converted_solution["y"]),
+        "partial_weekends": calculate_partial_weekends(candidate_solution, converted_solution["x"]),
+        "consecutive_days": calculate_consecutive_days(candidate_solution, converted_solution["x"]),
+        "isolated_off_days": calculate_isolated_off_days(candidate_solution, converted_solution["x"]),
+        "isolated_working_days": calculate_isolated_working_days(candidate_solution, converted_solution["x"]),
+        "deviation_contracted_hours": calculate_negative_deviation_from_contracted_hours(candidate_solution, converted_solution["y"])
+    }
+
+    hard_variables = {
+        "below_minimum_demand": {},
+        "above_maximum_demand": {},
+        "more_than_one_shift_per_day": {},
+        "cover_multiple_demand_periods": {},
+        "weekly_off_shift_error": {},
+        "no_work_during_off_shift": {},
+        "mapping_shift_to_demand": {},
+        "delta_positive_contracted_hours": {}
+    }
+    objective_function, f = calculate_objective_function(model, soft_variables)
+
+    state = State(converted_solution, soft_variables, hard_variables, objective_function, f)
 
     criterion = GreedyCriterion()
 
