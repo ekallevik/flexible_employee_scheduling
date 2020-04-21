ac80011caa55bba3d334f4255aa8995cf8f74114
Even <even.kallevik@gmail.com>
2020-04-21 13:49:43 +0200

    Implement SimulatedAnnealingCriterion

--- heuristic/criterions/greedy_criterion.py
+++ heuristic/criterions/greedy_criterion.py
@@ -4,6 +4,5 @@ from heuristic.criterions.abstract_criterion import AbstractCriterion
 class GreedyCriterion(AbstractCriterion):
     """ Accepts any candidate with an equal or better solution compared to current solution """
 
-    def accept(self, candidate, current):
-        # todo: is this correct according to the algorithm? -Even, 29. March
+    def accept(self, candidate, current, random_state):
         return candidate.get_objective_value() >= current.get_objective_value()
