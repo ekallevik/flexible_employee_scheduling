1a773098acb6dd63d785363b02c619e891f958c3
Even <even.kallevik@gmail.com>
2020-04-29 12:57:01 +0200

    Extracting code into smaller functions

--- preprocessing/preferences.py
+++ preprocessing/preferences.py
@@ -6,17 +6,18 @@ from preprocessing.xml_loader import *
 def generate_preferences(staff, time_set, num_weekly_preferences, preferences_dur):
     """
     Generates random preferences for each employee. The result is normalized to 1
-    Note: the result is identical between different runs, as using the same initial seed will guarantee the same result
+    Note: the result is identical between different runs, as using the same initial seed will
+    guarantee the same result
     :param staff: data structure containing employees
     :param time_set: data structure containing time_step, weeks and needed time_periods-sets
-    :param num_weekly_preferences: (min, max)-tuple representing bounds on the number of requests per employee per week
-    :param preferences_dur: a (min, max)-tuple representing bounds on the durations of the preference
+    :param num_weekly_preferences: (min, max)-tuple representing bounds on the number of requests
+    per employee per week
+    :param preferences_dur: (min, max)-tuple representing bounds on the durations of the preference
     :return: a dict with keys [e][t], representing employee and time respectively
     """
 
     # Ensuring persistent results between different runs. The seed can be any integer
     random.seed(1)
-    preferences = tupledict()
 
     employees = staff["employees"]
     time_step = time_set["step"]
@@ -26,13 +27,7 @@ def generate_preferences(staff, time_set, num_weekly_preferences, preferences_du
 
     # Duration in hours is converted to duration in time_steps
     preferences_dur = [int(preferences_dur[0]/time_step), int(preferences_dur[1]/time_step)]
-
-    # Initializing all preferences
-    for employee in employees:
-        preferences[employee] = tupledict()
-
-        for time in time_periods:
-            preferences[employee][time] = 0
+    preferences = initialize_preferences(employees, time_periods)
 
     # Generating preferences
     for employee in employees:
@@ -40,17 +35,23 @@ def generate_preferences(staff, time_set, num_weekly_preferences, preferences_du
 
         for week in weeks:
             used_time_periods = []
-            weekly_preferences = int(random.randint(num_weekly_preferences[0], num_weekly_preferences[1]))
-            realized_dur = [random.randint(preferences_dur[0], preferences_dur[1]) for i in range(weekly_preferences)]
+            weekly_preferences = int(random.randint(num_weekly_preferences[0],
+                                                    num_weekly_preferences[1]))
+            realized_dur = get_realized_durations(preferences_dur, weekly_preferences)
 
             for dur in realized_dur:
+
+                # Positive and negative values signifies preference for work and free,
+                # but unsure which value signifies what
                 value = random.choice([-1, 1])
+
                 unique_preference = False
 
                 # While-block is ensuring that preferences do not overlap each other
                 while not unique_preference:
                     day = random.randint(week * 7, ((week + 1) * 7) - 1)
                     unique_preference = True
+
                     try:
                         start_index = random.randint(0, len(time_periods_in_day[day]) - dur)
                     except:
@@ -68,11 +69,44 @@ def generate_preferences(staff, time_set, num_weekly_preferences, preferences_du
 
                 number_of_preferences += dur
 
-        factor = 1.0/number_of_preferences
+        preferences = normalize_preferences(employee, number_of_preferences, preferences)
 
-        # Normalizing the preferences
-        for elem in preferences[employee]:
-            preferences[employee][elem] *= factor
+    return preferences
+
+
+def normalize_preferences(employee, number_of_preferences, preferences):
+    """ Normalizes the preferences """
+
+    factor = 1.0 / number_of_preferences
+
+    for elem in preferences[employee]:
+        preferences[employee][elem] *= factor
+
+    return preferences
+
+
+def get_realized_durations(preferences_dur, weekly_preferences):
+    """
+    Generates durations for each of the preferences in weekly_preferences belonging to a particular
+    employee
+    """
+
+    realized_dur = [random.randint(preferences_dur[0], preferences_dur[1]) for _ in
+                    range(weekly_preferences)]
+
+    return realized_dur
+
+
+def initialize_preferences(employees, time_periods):
+    """ Initializing all preferences """
+
+    preferences = tupledict()
+
+    for employee in employees:
+        preferences[employee] = tupledict()
+
+        for time in time_periods:
+            preferences[employee][time] = 0
 
     return preferences
 
