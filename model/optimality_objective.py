240b360e4f3eeda925839bcc9dee51dbb3f645c9
Sondre Nærland <sondrenaerland@gmail.com>
2020-04-29 12:11:58 +0200

    Added normalization of contracted hours and weekly rest

--- model/optimality_objective.py
+++ model/optimality_objective.py
@@ -26,7 +26,7 @@ class OptimalityObjective:
             (
                 f["plus"][e] - f["minus"][e]
                 == weights["rest"] * quicksum(v * w[e, t, v] for t, v in self.off_shifts)
-                - weights["contracted hours"] * lam[e]
+                - weights["contracted hours"][e] * lam[e]
                 - weights["partial weekends"]
                 * quicksum(rho["sat"][e, i] + rho["sun"][e, i + 1] for i in self.saturdays)
                 - weights["isolated working days"]
--- utils/weights.py
+++ utils/weights.py
@@ -1,5 +1,6 @@
 
 from utils.const import WEEKLY_REST_DURATION
+from gurobipy.gurobipy import tupledict
 
 
 def set_weights():
@@ -21,12 +22,14 @@ def set_weights():
     }
 
 
-def update_relative_weights(weights, num_employees):
+def update_relative_weights(weights, staff):
     """
     Update weights so that they are relatively weighted against each other.
         * Lowest fairness score:    Multiplied with number of employees to match the accumulated fairness score.
         * Demand deviation:
     """
+
+    num_employees = len(staff["employees"])
 
     weights["lowest fairness score"] *= num_employees
     #weights["demand deviation"]
@@ -34,11 +37,38 @@ def update_relative_weights(weights, num_employees):
     return weights
 
 
-def update_normalized_weights(weights):
+def normalize_weekly_rest_weight(weights):
+
+    max_additional_weekly_rest = WEEKLY_REST_DURATION[1] - WEEKLY_REST_DURATION[0]
+    weights["rest"] /= max_additional_weekly_rest
+
+    return weights
+
+
+def normalize_contracted_hours_weight(weights, employee_contracted_hours, num_weeks):
+
+    contracted_hours_weights = tupledict()
+    normalized_deviation_percentage = 0.05
+
+    for e in employee_contracted_hours:
+        contracted_hours_weights[e] = weights["contracted hours"] / \
+                                      (num_weeks * employee_contracted_hours[e] * normalized_deviation_percentage)
+
+    weights["contracted hours"] = contracted_hours_weights
+
+    return weights
+
+
+def update_normalized_weights(weights, staff, time_set):
     """
-    Update weights so that they are normalized.
+    Update weights so that they are normalized to around 1.
+    Weekly rest
     """
-    # Todo: Implement
+    employee_contracted_hours = staff["employee_contracted_hours"]
+    num_weeks = len(time_set["weeks"])
+
+    weights = normalize_weekly_rest_weight(weights)
+    weights = normalize_contracted_hours_weight(weights, employee_contracted_hours, num_weeks)
 
     return weights
 
@@ -52,15 +82,10 @@ def get_shift_design_weights():
 
 def get_weights(staff, time_set):
 
-    num_employees = len(staff["employees"])
-    time_step = time_set["step"]
-    time_periods = time_set["periods"][0]
-    days = time_set["days"]
-    weeks = time_set["weeks"]
-
     weights = set_weights()
-    weights = update_relative_weights(weights, num_employees)
-    weights = update_normalized_weights(weights)
+    weights = update_relative_weights(weights, staff)
+    weights = update_normalized_weights(weights, staff, time_set)
+    print(weights["contracted hours"][1])
 
     return weights
 
