c6c6d63b487c5b457c7b90722005c8ff977e5604
Håkon Grov <hakongrov@gmail.com>
2020-05-02 19:40:44 +0200

    Adds files to prepare for merge

---
+++ preprocessing/demand.py
@@ -0,0 +1,47 @@
+from datetime import time, datetime, date
+class Demand():
+    
+
+    def __init__(self, demand_id):
+        self.today = date.today()
+        self.demand_id = demand_id
+        self.start = []
+        self.end = []
+        self.minimum = []
+        self.maks = [] 
+        self.ideal = []
+        self.time_delta = []
+        self.time_step_length = None
+    
+    def add_info(self, start, end, maksimum, minimum, ideal):
+        start = start.split(":")
+        end = end.split(":")
+        if (int(start[1]) != 0):
+            start = float(start[0]) + (100/(60/float(start[1])))/100
+        else:
+            start = float(start[0]) + float(start[1])
+        if(int(end[1]) != 0):
+            end =   float(end[0]) + (100/(60/float(end[1])))/100
+        else:
+            end = float(end[0]) + float(end[1])
+        self.start.append(start)
+        self.end.append(end)
+        self.minimum.append(int(minimum))
+        self.maks.append(int(maksimum))
+        self.ideal.append(int(ideal))
+        #self.time_delta.append(datetime.combine(self.today, end) - datetime.combine(self.today,start))
+
+    def add_info2(self, start, end, maksimum, minimum, ideal):
+        start = start.split(":")
+        end = end.split(":")
+        self.start.append(time(int(start[0]),int(start[1])))
+        self.end.append(time(int(end[0]), int(end[1])))
+        self.minimum.append(int(minimum))
+        self.maks.append(int(maksimum))
+        self.ideal.append(int(ideal))
+        #self.time_delta.append(datetime.combine(self.today, end) - datetime.combine(self.today,start))
+
+
+    def __str__(self):
+        return(self.demand_id)
+
---
+++ preprocessing/xml_loader.py
@@ -0,0 +1,127 @@
+import sys
+from pathlib import Path
+from xml_loader.demand import Demand
+from xml_loader.employee import Employee
+from xml_loader.rest_rule import Weekly_rest_rule, Daily_rest_rule
+from datetime import timedelta, datetime
+today = datetime.today()
+
+def get_demand_definitions(root):
+    demands = []
+    for DemandDefinition in root.findall('Demands/DemandDefinitions/DemandDefinition'):
+        dem = Demand(DemandDefinition.find("DayDemandId").text)
+        for row in DemandDefinition.find("Rows").findall("Row"):
+            start = row.find("TimeStart").text
+            end = row.find("TimeEnd").text
+            maksimum = row.find("Max").text
+            minimum = row.find("Min").text
+            ideal = row.find("Ideal").text
+            dem.add_info(start, end, maksimum, minimum, ideal)
+        demands.append(dem)
+    return demands
+
+def get_competencies(root):
+    competencies = []
+    for competence in root.findall('Configuration/Competences/Competence'):
+        id = competence.find('CompetenceId').text
+        competencies.append(id)
+    return competencies
+
+def get_days_with_demand(root):
+    demands = get_demand_definitions(root)
+    days_with_demand = {}
+    for day in root.findall('Demands/DayDemandList/DayDemand'):
+        for obj in demands:
+            if obj.demand_id == day.find("DayDemandId").text:
+                d = int(day.find("DayIndex").text)
+                days_with_demand[d] = obj
+    return days_with_demand
+
+def get_demand_definitions2(root):
+    demands = []
+    for DemandDefinition in root.findall('Demands/DemandDefinitions/DemandDefinition'):
+        dem = Demand(DemandDefinition.find("DayDemandId").text)
+        for row in DemandDefinition.find("Rows").findall("Row"):
+            start = row.find("TimeStart").text
+            end = row.find("TimeEnd").text
+            maksimum = row.find("Max").text
+            minimum = row.find("Min").text
+            ideal = row.find("Ideal").text
+            dem.add_info2(start, end, maksimum, minimum, ideal)
+        demands.append(dem)
+    return demands
+
+def get_days_with_demand2(root):
+    demands = get_demand_definitions2(root)
+    days_with_demand = {}
+    for day in root.findall('Demands/DayDemandList/DayDemand'):
+        for obj in demands:
+            if obj.demand_id == day.find("DayDemandId").text:
+                day_obj = today + timedelta(days = int(day.find("DayIndex").text))
+                days_with_demand[day_obj] = obj
+    return days_with_demand
+
+def get_days(root):
+    days = []
+    for day in root.findall('Demands/DayDemandList/DayDemand'):
+        days.append(int(day.find("DayIndex").text))
+    return days
+
+def get_weekly_rest_rules(root):
+    weekly_rest_rules = []
+    for weekly_rule in root.findall('Configuration/WeeklyRestRules/WeeklyRestRule'):
+        rest_id = weekly_rule.find("Id").text
+        hours = weekly_rule.find("MinRestHours").text
+        rule = Weekly_rest_rule(hours, rest_id)
+        weekly_rest_rules.append(rule)
+    return weekly_rest_rules
+
+def get_daily_rest_rules(root):
+    daily_rest_rules = []
+    for daily_rule in root.findall('Configuration/DayRestRules/DayRestRule'):
+        rest_id = daily_rule.find("Id").text
+        hours = daily_rule.find("MinRestHours").text
+        rule = Daily_rest_rule(hours, rest_id)
+        daily_rest_rules.append(rule)
+    return daily_rest_rules
+
+def get_employees(root, competencies):
+    weekly_rest_rules = get_weekly_rest_rules(root)
+    Daily_rest_rules = get_daily_rest_rules(root)
+    employees = []
+    for schedule_row in root.findall('SchedulePeriod/ScheduleRows/ScheduleRow'):
+        employee_id = schedule_row.find("RowNbr").text
+        emp = Employee(employee_id)
+        try: 
+            contracted_hours = float(schedule_row.find("WeekHours").text)
+        except AttributeError:
+            print("ScheduleRow %s don't have a set WeekHours tag" % employee_id)
+            contracted_hours = 36
+
+        if(len(competencies) == 0):
+            emp.set_comptency(0)
+        else:
+            try: 
+                for competence in schedule_row.find("Competences").findall("CompetenceId"):
+                    if(competence.text in competencies):
+                        emp.set_comptency(competence.text)
+            except AttributeError:
+                print("ScheduleRow %s don't have a set Competence tag" % employee_id)
+        try:
+            weekly_rest_rule = schedule_row.find("WeeklyRestRule").text
+            for weekly_rule in weekly_rest_rules:
+                if(weekly_rule.rest_id == weekly_rest_rule):
+                    emp.add_weekly_rest(weekly_rule.hours)
+        except:
+            pass
+        try:
+            daily_rest_rule = schedule_row.find("DayRestRule").text
+            for daily_rule in Daily_rest_rules:
+                if(daily_rule.rest_id == daily_rest_rule):
+                    emp.add_daily_rest(daily_rule.hours)
+        except:
+            pass
+        
+        emp.set_contracted_hours(contracted_hours)
+        employees.append(emp)
+    return employees
