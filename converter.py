8982e2341d61792d1f9abc2c493da9c1b9aefd56
Even <even.kallevik@gmail.com>
2020-05-02 19:03:54 +0200

    Fix buggy import

--- converter.py
+++ converter.py
@@ -1,15 +1,15 @@
-from tupledict import TupleDict
 
 def convert(model):
-    x = {(e,t,v): abs(model.x[e,t,v].x) for e,t,v in model.x}
-    y = {(c,e,t): abs(model.y[c,e,t].x) for c,e,t in model.y}
-    w = {(e,j): (t,v) for e in model.employees for j in model.weeks for t,v in model.off_shift_in_week[j] if model.w[e,t,v].x == 1}
-    return x,y,w
+    x = {(e, t, v): abs(model.x[e, t, v].x) for e, t, v in model.x}
+    y = {(c, e, t): abs(model.y[c, e, t].x) for c, e, t in model.y}
+    w = {(e, j): (t, v) for e in model.employees for j in model.weeks for t,
+                            v in model.off_shift_in_week[j] if model.w[e, t, v].x == 1}
+    return x, y, w
 
 
 def set_x(state, t_covered_by_shift, e, t, v, value):
-    state.x[e,t,v] = value
+    state.x[e, t, v] = value
     #Need a smarter solution for choosing competency
-    for t1 in t_covered_by_shift[t,v]:
-        state.y[0,e,t1] = value
-    return (e,t,v)
+    for t1 in t_covered_by_shift[t, v]:
+        state.y[0, e, t1] = value
+    return (e, t, v)
