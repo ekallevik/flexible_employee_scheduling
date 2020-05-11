e2849d46415f1d2062c0d265ec265605180a24fc
Even <even.kallevik@gmail.com>
2020-05-11 17:13:19 +0200

    Adding __str__ to criterions

--- heuristic/criterions/abstract_criterion.py
+++ heuristic/criterions/abstract_criterion.py
@@ -1,5 +1,8 @@
 class AbstractCriterion:
     """ Abstract-class to make sure that every criterion implements the necessary methods"""
 
+    def __str__(self):
+        raise NotImplementedError
+
     def accept(self, candidate, current, random_state):
         raise NotImplementedError
