995b3b4ef95cdf43716eafb331b08bf324d1ae77
Håkon Grov <hakongrov@gmail.com>
2020-05-13 16:54:11 +0200

    Fixes bug in illegal week swap

--- heuristic/local_search_operators.py
+++ heuristic/local_search_operators.py
@@ -7,6 +7,7 @@ from random import choice
 def illegal_week_swap(shifts_in_week, employees, shifts_at_day, t_covered_by_shift, competencies, contracted_hours, time_periods_in_week, combined_time_periods_in_week, time_step, L_C_D, weeks, state):
     destroy_set = []
     repair_set = []
+    already_fixed_employees = []
     for emp, j in state.hard_vars["weekly_off_shift_error"]:
         if state.hard_vars["weekly_off_shift_error"][emp,j] == 1:
             shifts = [(t, v) for t, v in shifts_in_week[j] if state.x[emp,t,v] != 0]
@@ -15,6 +16,9 @@ def illegal_week_swap(shifts_in_week, employees, shifts_at_day, t_covered_by_shi
             objective_values = {}
             for shift in shifts:
                 current_state = state.copy()
+                
+                possible_employees = [e for e in employees if sum(current_state.x[e,t,v] for t,v in shifts_at_day[int(shift[0]/24)]) == 0 and (e,j) not in already_fixed_employees]
+
                 set_x(current_state, t_covered_by_shift, emp, shift[0], shift[1], 0)
 
                 calculate_weekly_rest(current_state, shifts_in_week, [emp], [j])
@@ -24,7 +28,7 @@ def illegal_week_swap(shifts_in_week, employees, shifts_at_day, t_covered_by_shi
                 calculate_consecutive_days(current_state, [emp], shifts_at_day, L_C_D, days_in_week)
                 delta_calculate_negative_deviation_from_contracted_hours(current_state, [emp], contracted_hours, weeks, time_periods_in_week, competencies, time_step)
 
-                possible_employees = [e for e in employees if (sum(current_state.x[e,t,v] for t,v in shifts_at_day[int(shift[0]/24)])) == 0]
+                
                 #if sum(state.soft_vars["contracted_hours"][e,j] for j in weeks) - shift[1] >= 0
                 if len(possible_employees) == 0:
                     print("Not enough employees")
@@ -49,14 +53,23 @@ def illegal_week_swap(shifts_in_week, employees, shifts_at_day, t_covered_by_shi
                     #Should not be needed: below_minimum_demand(current_state, repaired, employee_with_competencies, demand, competencies, t_covered_by_shift)
 
                     #Calculate the objective function when the employee e is assigned the shift
-                    objective_values[e_p, shift] = calc_weekly_objective_function(state, competencies, time_periods_in_week, combined_time_periods_in_week, employees, [j], L_C_D)[0]
+                    objective_values[e_p, shift] = calc_weekly_objective_function(current_state, competencies, time_periods_in_week, combined_time_periods_in_week, employees, [j], L_C_D)[0]
 
                     set_x(current_state, t_covered_by_shift, e_p, shift[0], shift[1], 0)
 
-                #set_x(current_state, t_covered_by_shift, emp, shift[0], shift[1], 1)
+                    calculate_weekly_rest(current_state, shifts_in_week, [e_p], [j])
+                    calculate_partial_weekends(current_state, [e_p], shifts_at_day, saturdays)
+                    calculate_isolated_working_days(current_state, [e_p], shifts_at_day, days_in_week)
+                    calculate_isolated_off_days(current_state, [e_p], shifts_at_day, days_in_week)
+                    calculate_consecutive_days(current_state, [e_p], shifts_at_day, L_C_D, days_in_week)
+                    delta_calculate_negative_deviation_from_contracted_hours(current_state, [e_p], contracted_hours, weeks, time_periods_in_week, competencies, time_step)
+
+                set_x(current_state, t_covered_by_shift, emp, shift[0], shift[1], 1)
             print(objective_values)
             max_value = max(objective_values.items(), key=itemgetter(1))[1]
             employee = choice([key for key, value in objective_values.items() if value == max_value])
+
+            already_fixed_employees.append((emp, j))
 
             repair_set.append(set_x(state, t_covered_by_shift, employee[0], employee[1][0], employee[1][1], 1))
             destroy_set.append(remove_x(state, t_covered_by_shift, competencies, emp, employee[1][0], employee[1][1]))
