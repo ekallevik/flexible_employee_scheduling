7702112b0b37b4bbe6e3ef4c797d04c3c5f96cbe
Sondre Nærland <sondrenaerland@gmail.com>
2020-05-20 22:46:54 +0200

    Implemented implicit model

---
+++ model/implicit_model.py
@@ -0,0 +1,63 @@
+
+from loguru import logger
+
+from model.implicit_variables import ImplicitVariables
+from model.implicit_constraints import ImplicitConstraints
+from model.implicit_objective import ImplicitObjective
+from utils.weights import get_weights
+
+
+class ImplicitModel:
+
+    def __init__(self, model, data):
+
+        self.model = model
+        self.weights = get_weights(data["time"], data["staff"])
+
+        # Retrieve relevant data from "data"
+        self.employees = data["staff"]["employees"]
+        self.combined_time_periods = data["time"]["combined_time_periods"][0]
+        self.time_periods = data["time"]["periods"][0]
+        self.competencies = data["competencies"]
+        self.days = data["time"]["days"]
+        self.preferences = data["preferences"]
+        self.shift_durations = data["shift_durations"]
+
+        self.var = ImplicitVariables(
+            model=self.model,
+            employees=self.employees,
+            combined_time_periods=self.combined_time_periods,
+            time_periods=self.time_periods,
+            shift_durations=self.shift_durations,
+            competencies=self.competencies,
+            days=self.days
+        )
+
+        self.constraints = ImplicitConstraints(
+            model=self.model,
+            var=self.var,
+            data=data,
+        )
+
+        self.objective = ImplicitObjective(
+            model=self.model,
+            var=self.var,
+            weights=self.weights,
+            competencies=self.competencies,
+            preferences=self.preferences,
+            staff=data["staff"],
+            time_set=data["time"],
+            shift_durations=self.shift_durations
+        )
+
+    def run_model(self):
+        self.model.optimize()
+        logger.error(f"Model is {self.model.status}")
+
+    def get_variables(self):
+        """ Return vars used in the model """
+        return self.var
+
+    def get_objective_value(self):
+        """ Returns the object value of the found solution """
+        return self.model.getObjective().getValue()
