23b859e2bbfc64ee1f30a60769fcc3b36bf87ec7
Even Kallevik <even.kallevik@gmail.com>
2020-05-26 22:00:34 +0200

    Add simple tuning

--- main.py
+++ main.py
@@ -338,6 +338,110 @@ class ProblemRunner:
         return self.log_name
 
 
+
+def evaluate_parameters(search_params):
+
+    pr = ProblemRunner()
+
+    breakpoint()
+
+    decay = search_params["decay"]
+    operator_weights = {
+        "IS_REJECTED": search_params["IS_REJECTED"],
+        "IS_ACCEPTED": search_params["IS_ACCEPTED"],
+        "IS_BETTER": search_params["IS_BETTER"],
+        "IS_BEST": search_params["IS_BEST"],
+    }
+
+    pr.alns.decay = decay
+    pr.alns.WeightUpdate = operator_weights
+
+    pr.run_alns(runtime=1)
+
+    score = pr.alns.get_best_solution_value()
+
+    return score
+
+def objective(**params):
+    return -1.0 * evaluate_parameters(params)
+
+def tune_hyperparameters():
+
+    SPACE = [
+        skopt.space.Real(0.01, 0.99, name='decay', prior='uniform'),
+        #skopt.space.Integer(1, 30, name='max_depth'),
+        skopt.space.Real(0.5, 1.0, name='is_rejected', prior='uniform'),
+        skopt.space.Real(1.0, 1.3, name='is_accepted', prior='uniform'),
+        skopt.space.Real(1.2, 1.5, name='is_better', prior='uniform'),
+        skopt.space.Real(1.4, 1.8, name='is_best', prior='uniform'),
+    ]
+
+    results = skopt.forest_minimize(objective, SPACE, n_calls=5, n_random_starts=1)
+    best_auc = -1.0 * results.fun
+    best_params = results.x
+
+    print('best result: ', best_auc)
+    print('best parameters: ', best_params)
+
+    print("Results")
+    pprint(results)
+
+    breakpoint()
+
+
+
+def run_multiple_problems(variant=0, threads=32, runtime=15):
+
+    problems = [
+        "rproblem1",
+        "rproblem2",
+        "rproblem3",
+        "rproblem4",
+        "rproblem5",
+        "rproblem6",
+        "rproblem7",
+        "rproblem8",
+        "rproblem9",
+        "rproblem3_2_weeks",
+        "rproblem3_8_weeks",
+        "rproblem5_4_weeks",
+        "rproblem5_12_weeks",
+        "rproblem6_8_weeks",
+        "rproblem6_16_weeks",
+        "rproblem7_8_weeks",
+        "rproblem7_16_weeks",
+        "rproblem9_8_weeks",
+        "rproblem9_16_weeks",
+    ]
+
+    if variant == 0:
+        problems = ["rproblem9"]
+        share_times = None
+    if variant == 1:
+        problems = ["rproblem9"]
+        share_times = [i for i in range(60, 15 * 60, 10)]
+    if variant == 2:
+        problems = ["rproblem9"]
+        share_times = [i for i in range(60, 15 * 60, 30)]
+    if variant == 3:
+        problems = ["rproblem8"]
+
+    share_time_list = [
+        [i for i in range(60, 15 * 60, 10)],
+        [i for i in range(60, 15 * 60, 30)],
+        [i for i in range(60, 15 * 60, 45)],
+        [i for i in range(60, 15 * 60, 60)]
+                   ]
+
+    #for share_times in share_time_list:
+
+    for problem in problems:
+        logger.critical(f"Running {problem} with {threads} threads and shares=\n{share_times}")
+
+        pr = ProblemRunner(problem=problem)
+        pr.run_palns(share_times=share_times, threads=threads, runtime=runtime)
+        logger.critical(f"Completed run of {problem} with {threads} threads and shares=\n{share_times}")
+
 if __name__ == "__main__":
     """ 
     Run any function with arguments ARGS by using:
