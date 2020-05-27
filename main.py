014fc5e7bf31f0999dd247b87735f2a482333376
Even Kallevik <even.kallevik@gmail.com>
2020-05-27 20:05:55 +0200

    Add Neptune

--- main.py
+++ main.py
@@ -389,6 +389,20 @@ def tune_hyperparameters():
     breakpoint()
 
 
+def neptune():
+    import neptune
+
+    # The init() function called this way assumes that
+    # NEPTUNE_API_TOKEN environment variable is defined.
+
+    neptune.init('ekallevik/sandbox')
+    neptune.create_experiment(name='minimal_example')
+
+    # log some metrics
+    for i in range(100):
+        neptune.log_metric('loss', 0.95 ** i)
+
+    neptune.log_metric('AUC', 0.96)
 
 def run_multiple_problems(variant=0, threads=32, runtime=15):
 
