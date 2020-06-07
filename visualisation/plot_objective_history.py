83cfbf0e5e2f41a37321714f946469f6f9ba1e1b
Even <even.kallevik@gmail.com>
2020-06-07 21:58:32 +0200

    Add solution visualizations

--- visualisation/plot_objective_history.py
+++ visualisation/plot_objective_history.py
@@ -1,34 +1,34 @@
 import json
-from bisect import bisect_right, bisect_left
+from bisect import bisect_left
 from math import inf
 
 import matplotlib.pyplot as plt
 from loguru import logger
 
 files = {
-    "problem1": "results/variance_3_representative/2020-06-06_20:01:09-rproblem1_mode=feasibility_sdp_reduce-seed=200.json",
-    "problem2": "results/variance_3_representative/2020-06-06_20:01:20-rproblem2_mode=feasibility_sdp_reduce-seed=500.json",
-    "problem3": "results/variance_3_representative/2020-06-06_20:01:26-rproblem3_mode=feasibility_sdp_reduce-seed=900.json",
-    "problem4": "results/variance_3_representative/2020-06-06_20:04:24-rproblem4_mode=feasibility_sdp_reduce-seed=600.json",
-    "problem5": "results/variance_3_representative/2020-06-06_20:01:36-rproblem5_mode=feasibility_sdp_reduce-seed=100.json",
-    "problem6": "results/variance_3_representative/2020-06-06_20:01:41-rproblem6_mode=feasibility_sdp_reduce-seed=0.json",
-    "problem7": "results/variance_3_representative/2020-06-06_20:01:50-rproblem7_mode=feasibility_sdp_reduce-seed=200.json",
+    "problem1": "results/variance_3_representative/2020-06-06_20:01:09-rproblem1_mode=feasibility_sdp_reduce-seed=200",
+    "problem2": "results/variance_3_representative/2020-06-06_20:01:20-rproblem2_mode=feasibility_sdp_reduce-seed=500",
+    "problem3": "results/variance_3_representative/2020-06-06_20:01:26-rproblem3_mode=feasibility_sdp_reduce-seed=900",
+    "problem4": "results/variance_3_representative/2020-06-06_20:04:24-rproblem4_mode=feasibility_sdp_reduce-seed=300",
+    "problem5": "results/variance_3_representative/2020-06-06_20:01:36-rproblem5_mode=feasibility_sdp_reduce-seed=100",
+    "problem6": "results/variance_3_representative/2020-06-06_20:01:41-rproblem6_mode=feasibility_sdp_reduce-seed=0",
+    "problem7": "results/variance_3_representative/2020-06-06_20:01:50-rproblem7_mode=feasibility_sdp_reduce-seed=200",
     #"problem8": "results/2020-06-01_22:28:08-rproblem8_mode=feasibility_sdp_reduce.json",
-    "problem9": "results/variance_3_representative/2020-06-06_20:02:10-rproblem9_mode=feasibility_sdp_reduce-seed=300.json",
+    "problem9": "results/variance_3_representative/2020-06-06_20:02:10-rproblem9_mode=feasibility_sdp_reduce-seed=300",
 }
 
 workers = [f"worker-{j}" for j in range(48)]
 
 def plot_best(step=1):
 
-    for problem, file in files.items():
+    for problem, filename in files.items():
 
-        with open(file) as f:
+        with open(f"{filename}.json") as f:
             data = json.load(f)
 
-        construction_runtime = data["_time"]["runtime_construction"]
+        construction_runtime = int(data["_time"]["runtime_construction"]) + 1
 
-        worker_times = [i for i in range(step, 901-construction_runtime, step)]
+        worker_times = [i for i in range(step, 900-construction_runtime, step)]
         times = [i for i in range(900)]
         best_list = [-inf for _ in range(step, construction_runtime, step)]
 
@@ -49,17 +49,21 @@ def plot_best(step=1):
 
             best_list.append(best)
 
+        diff = len(times) - len(best_list)
+        for _ in range(diff):
+            best_list.append(best_list[-1])
+
         result = {
             "times": times,
             "best_list": best_list
                   }
 
-        with open(f"results/{problem}-best_list.json", "w") as fp:
+        with open(f"{filename}-best.json", "w") as fp:
             json.dump(result, fp, sort_keys=True, indent=4)
 
-        plt.plot(worker_times, best_list, "g-", markersize=6)
+        plt.plot(times, best_list, "g-", markersize=6)
         plt.title(problem)
-        plt.show()
+        plt.savefig(filename)
 
 def plot_history():
 
