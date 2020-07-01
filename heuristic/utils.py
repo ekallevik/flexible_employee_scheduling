d81e69dd20d2fde27809b1c83944828ccc3c8866
Even <even.kallevik@gmail.com>
2020-07-01 15:55:33 +0200

    Delete deprecated utils

--- heuristic/utils.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class WeightUpdate(Enum):
-    # todo: we should add proper values here, so we can use them in weight-updating
-    IS_BEST = 1.3
-    IS_BETTER = 1.2
-    IS_ACCEPTED = 1.1
-    IS_REJECTED = 0.9
